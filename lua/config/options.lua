c5267bb9c8186fed8dc94077683189a6993f524f
xuwei152 <xuwei@ihep.ac.cn>
2025-02-28 11:17:29 +0800

    feat(config): add new Vim options for improved
    - Set command height to0 for a cleaner interface.
    - Enable relative line numbers for better navigation.
    - Configure GUI cursor styles for different modes.
    - Set clipboard to use 'unnamedplus' system clipboard integration.
    - Increase history size to 10000 for better command recall.
    - Append shada settings to retain more command history.
    - Configure xclip for clipboard operations with xsel.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.cmdheight = 0
+vim.opt.relativenumber = true
+vim.opt.guicursor =
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.o.clipboard = "unnamedplus"
+vim.o.history = 10000
+vim.opt.shada:append("'1000")
+
+vim.g.clipboard = {
+  name = "xclip",
+  copy = {
+    ["+"] = "xsel -ib",
+    ["*"] = "xsel -ib",
+  },
+  paste = {
+    ["+"] = "xsel -ob",
+    ["*"] = "xsel -ob",
+  },
+  cache_enabled = true,
+}
