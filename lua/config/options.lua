e55b9d6f12689c05c8f6d0354449f5a3177dff3d
xuwei152 <xuwei@ihep.ac.cn>
2025-02-28 22:53:10 +0800

    add noswap opt

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,7 @@ vim.opt.guicursor =
   "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.o.clipboard = "unnamedplus"
 vim.o.history = 10000
+vim.o.swapfile = false
 vim.opt.shada:append("'1000")
 
 vim.g.clipboard = {
